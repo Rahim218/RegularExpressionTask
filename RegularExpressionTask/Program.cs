f44a774a990a0e66fe76b64c6917b4ea1b1d9dc0
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-01-08 16:02:00 +0400

    Add project files.

---
+++ RegularExpressionTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+#region PhoneNumber
+//string phoneNumber = Console.ReadLine();
+//+994 50 265 64 63
+//Regex regex = new Regex(@"(^(^\+994)\s?(50|51|55|77))\s?(\d{3}?)\s?(\d{2}?)\s?(\d{2}?)");
+//Match match = regex.Match(phoneNumber);
+//Console.WriteLine(match.Success);
+#endregion
+
+
+#region DateTime
+//string dateTime = Console.ReadLine();
+//dd.MM.yyyy
+//Regex regex = new Regex(@"^(3[0-1]|[0-1][1-9]|0?[0-9])\.?(0?[1-9]|1[0-2])\.([0-9]{4})");
+//Match match = regex.Match(dateTime);
+//Console.WriteLine(match.Success);
+#endregion
+
+
+

edf0139855ee3f273ed9b8f5037cc1889267ddd3
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-01-09 16:11:22 +0400

    new

--- RegularExpressionTask/Program.cs
+++ RegularExpressionTask/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Text.RegularExpressions;
 #region PhoneNumber
-//string phoneNumber = Console.ReadLine();
+string phoneNumber = Console.ReadLine();
 //+994 50 265 64 63
-//Regex regex = new Regex(@"(^(^\+994)\s?(50|51|55|77))\s?(\d{3}?)\s?(\d{2}?)\s?(\d{2}?)");
-//Match match = regex.Match(phoneNumber);
-//Console.WriteLine(match.Success);
+Regex regex = new Regex(@"(^(^\+994)\s?(50|51|55|77))\s?(\d{3})\s?(\d{2})\s?(\d{2})");
+Match match = regex.Match(phoneNumber);
+Console.WriteLine(match.Success);
 #endregion
 
 
